2ce50787983f738136d4fc400efe5c6d6915b09c
Izhanitov <dm.define@gmail.com>
2022-12-04 21:25:49 +0700

    Troubles with imageModal fixed

--- src/components/imageModal/imageModal.js
+++ src/components/imageModal/imageModal.js
@@ -1,23 +1,21 @@
 const ImageModal = ({item, onClose}) => {
     const renderModal = () => {        
         const {url, name} = item;
-        console.log(url);
-        console.log(name);
         return (<>
-            <div class="modal fade show d-block" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg">
-              <div class="modal-content" > 
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">{name}</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" onClick={onClose} aria-label="Close"></button>
+            <div className="modal fade show d-block" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" onClick={onClose}>
+            <div className="modal-dialog modal-lg">
+              <div className="modal-content" > 
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="exampleModalLabel">{name}</h1>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" onClick={onClose} aria-label="Close"></button>
                 </div>
-                <div class="modal-body text-center">
+                <div className="modal-body text-center">
                   <img src={url} className="modal-img" alt={name}/>
                 </div>
               </div>
             </div>
           </div>
-          <div onClick={onClose} class="modal-backdrop fade show"></div>
+          <div onClick={onClose} className="modal-backdrop fade show"></div>
           </>
         );
     }
